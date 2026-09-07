9513fe7d07aea1a00aaca2156b9df307876eca11
Firuz Chamarov <firuzchamarov@gmail.com>
2026-09-07 07:59:09 +0500

    Initial Commit

---
+++ ListingApp/ListingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListingAppApp.swift
+//  ListingApp
+//
+//  Created by Macbook on 07.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct ListingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
