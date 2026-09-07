444f1331c2555a0c70faf71ea65ee0a0bbe995b1
Firuz Chamarov <firuzchamarov@gmail.com>
2026-09-07 17:01:38 +0500

    working on tabviews for the main menu

--- ListingApp/ContentView.swift
+++ ListingApp/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Home", systemImage: "house") {
+                Text("Главная")
+            }
+            Tab("Избранное", systemImage: "heart.fill") {
+                Text("Избранное")
+            }
+            Tab("Добавить", systemImage: "plus.circle.fill") {
+                
+            }
         }
-        .padding()
     }
 }
 
